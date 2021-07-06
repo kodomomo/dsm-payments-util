02fd603cd0b91928fc7f4ad21b9ed4b206653314
minki son <migsking@naver.com>
2021-07-06 10:49:54 +0900

    [CREATE] Booth Info

---
+++ booth_info.py
@@ -0,0 +1,86 @@
+booths = [
+    {
+        "booth_id": "booth_1",
+        "booth_name": "홈베이스",
+    },
+    {
+        "booth_id": "booth_2",
+        "booth_name": "누름꽃방향제 만들기",
+    },
+    {
+        "booth_id": "booth_3",
+        "booth_name": "학부모""평생교육",
+    },
+    {
+        "booth_id": "booth_4",
+        "booth_name": "뷰티풀마인드",
+    },
+    {
+        "booth_id": "booth_5",
+        "booth_name": "Anor Londo",
+    },
+    {
+        "booth_id": "booth_6",
+        "booth_name": "빙수세끼",
+    },
+    {
+        "booth_id": "booth_7",
+        "booth_name": "인생이 달고나",
+    },
+    {
+        "booth_id": "booth_8",
+        "booth_name": "뽀까뽀까",
+    },
+    {
+        "booth_id": "booth_9",
+        "booth_name": "피 한번 빨아 Bar",
+    },
+    {
+        "booth_id": "booth_10",
+        "booth_name": "메이드 포차",
+    },
+    {
+        "booth_id": "booth_11",
+        "booth_name": "라면 먹고 갈래",
+    },
+    {
+        "booth_id": "booth_12",
+        "booth_name": "2차는 여기로",
+    },
+    {
+        "booth_id": "booth_13",
+        "booth_name": "잔은 높게 우리 사이는 깊게",
+    },
+    {
+        "booth_id": "booth_14",
+        "booth_name": "ESD",
+    },
+    {
+        "booth_id": "booth_15",
+        "booth_name": "맛대똥",
+    },
+    {
+        "booth_id": "booth_16",
+        "booth_name": "DudePerfect",
+    },
+    {
+        "booth_id": "booth_17",
+        "booth_name": "인생사진",
+    },
+    {
+        "booth_id": "booth_18",
+        "booth_name": "코인노래방",
+    },
+    {
+        "booth_id": "booth_19",
+        "booth_name": "안녕? 나랑 같이 5층 갈래? (학생회부스)",
+    },
+    {
+        "booth_id": "booth_20",
+        "booth_name": "더 지니어스 (학생회부스)",
+    },
+    {
+        "booth_id": "booth_21",
+        "booth_name": "게임 광장 (학생회부스)",
+    },
+]
