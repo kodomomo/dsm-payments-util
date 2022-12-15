6bc0fd2d890ac2f98f010229381baefbcad4720e
osangu <hhgnoon@naver.com>
2022-12-15 15:43:39 +0900

    UPDATE: 2022 부스 리스트

--- booth_info.py
+++ booth_info.py
@@ -1,86 +1,54 @@
 booths = [
     {
         "booth_id": "booth_1",
-        "booth_name": "홈베이스",
+        "booth_name": "타로 맛집",
     },
     {
         "booth_id": "booth_2",
-        "booth_name": "누름꽃방향제 만들기",
+        "booth_name": "민이스톱",
     },
     {
         "booth_id": "booth_3",
-        "booth_name": "학부모""평생교육",
+        "booth_name": "세연이네 떡볶이",
     },
     {
         "booth_id": "booth_4",
-        "booth_name": "뷰티풀마인드",
+        "booth_name": "유성구 칼바람 김동하네 고기집",
     },
     {
         "booth_id": "booth_5",
-        "booth_name": "Anor Londo",
+        "booth_name": "ON",
     },
     {
         "booth_id": "booth_6",
-        "booth_name": "빙수세끼",
+        "booth_name": "페페 스튜디오",
     },
     {
         "booth_id": "booth_7",
-        "booth_name": "인생이 달고나",
+        "booth_name": "우리 집에서 라면 먹고 갈래?",
     },
     {
         "booth_id": "booth_8",
-        "booth_name": "뽀까뽀까",
+        "booth_name": "아이스크림 와플",
     },
     {
         "booth_id": "booth_9",
-        "booth_name": "피 한번 빨아 Bar",
+        "booth_name": "명석, 찬홍이와 함께하는 코스프레 포토부스!!",
     },
     {
         "booth_id": "booth_10",
-        "booth_name": "메이드 포차",
+        "booth_name": "마 자신있나",
     },
     {
         "booth_id": "booth_11",
-        "booth_name": "라면 먹고 갈래",
+        "booth_name": "은진BUKS",
     },
     {
         "booth_id": "booth_12",
-        "booth_name": "2차는 여기로",
+        "booth_name": "부모님과 함께 하는 체험 부스",
     },
     {
         "booth_id": "booth_13",
-        "booth_name": "잔은 높게 우리 사이는 깊게",
-    },
-    {
-        "booth_id": "booth_14",
-        "booth_name": "ESD",
-    },
-    {
-        "booth_id": "booth_15",
-        "booth_name": "맛대똥",
-    },
-    {
-        "booth_id": "booth_16",
-        "booth_name": "DudePerfect",
-    },
-    {
-        "booth_id": "booth_17",
-        "booth_name": "인생사진",
-    },
-    {
-        "booth_id": "booth_18",
-        "booth_name": "코인노래방",
-    },
-    {
-        "booth_id": "booth_19",
-        "booth_name": "안녕? 나랑 같이 5층 갈래? (학생회부스)",
-    },
-    {
-        "booth_id": "booth_20",
-        "booth_name": "더 지니어스 (학생회부스)",
-    },
-    {
-        "booth_id": "booth_21",
-        "booth_name": "게임 광장 (학생회부스)",
-    },
+        "booth_name": "학생 외 출입금지",
+    }
 ]
